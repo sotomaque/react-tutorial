724af5c738115c0efcc6a8d0816001a90f9484a0
Enrique <sotomaque@gmail.com>
2021-04-19 10:56:47 -0700

    basic story board learnings

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Button from './Button';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'form/Button',
   component: Button,
 };
 
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
+const Template = (args) => (
+  <Button onClick={action('Click handler')} {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+  children: 'Primary Args',
+};
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: 'secondary',
+  children: 'Secondary Args',
+};
+export const Success = Template.bind({});
+Success.args = {
+  variant: 'success',
+  children: 'Success Args',
+};
+export const Danger = Template.bind({});
+Danger.args = {
+  variant: 'danger',
+  children: 'Danger Args',
+};
---
+++ src/components/Center/Center.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Center.css';
+
+const Center = ({ children }) => <div className="center">{children}</div>;
+
+export default Center;
